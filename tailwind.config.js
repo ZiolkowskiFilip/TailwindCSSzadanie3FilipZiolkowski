271a2b96d88d1eaef14110c8fbfa302bc99280ea
ZiolkowskiFilip <146524603+ZiolkowskiFilip@users.noreply.github.com>
2023-10-19 12:36:44 +0200

    Add files via upload
    My solution in index.html

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["/Desktop/frameworki-css-homework-master.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
